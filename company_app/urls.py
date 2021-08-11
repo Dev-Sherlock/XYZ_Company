81f1e672e26f4dd15e0f83178f1ef9bcb224c9c4
Dev-Sherlock <khanbayov.tamerlan@gmail.com>
2021-08-11 15:15:54 +0200

    added index url to navbar

--- company_app/urls.py
+++ company_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import main,ProductListView
 
 urlpatterns = [
-    path('', main),
+    path('', main,name='index'),
     path('products/', ProductListView.as_view(), name='products'),
 ]
 
